1521428abcc8137cc9475e2b5171fa0e3b931196
jsm0325 <jsm961207@naver.com>
2024-05-18 18:13:40 +0900

    animation rigging 추가 및 풀트래킹 아바타 추가

---
+++ Assets/VR Body/IKTargetFollowVRRig.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class VRMap
+{
+    public Transform vrTarget;
+    public Transform ikTarget;
+    public Vector3 trackingPositionOffset;
+    public Vector3 trackingRotationOffset;
+    public void Map()
+    {
+        ikTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        ikTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+    }
+}
+
+public class IKTargetFollowVRRig : MonoBehaviour
+{
+    [Range(0,1)]
+    public float turnSmoothness = 0.1f;
+    public IKFootSolver leftIKFootSolver;
+    public IKFootSolver rightIKFootSolver;
+    public bool onFullTracking = true;
+    public VRMap head;
+    public VRMap leftHand;
+    public VRMap rightHand;
+    public VRMap Wrist;
+    public VRMap leftFoot;
+    public VRMap rightFoot;
+
+    public Vector3 headBodyPositionOffset;
+    public float headBodyYawOffset;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = head.ikTarget.position + headBodyPositionOffset;
+        float yaw = head.vrTarget.eulerAngles.y;
+        transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(transform.eulerAngles.x, yaw, transform.eulerAngles.z),turnSmoothness);
+
+        head.Map();
+        leftHand.Map();
+        rightHand.Map();
+        if(onFullTracking == true)
+        {
+            if (leftIKFootSolver.enabled == true)
+            {
+                leftIKFootSolver.enabled = false;
+                rightIKFootSolver.enabled = false;
+            }
+            Wrist.Map();
+            leftFoot.Map();
+            rightFoot.Map();
+        }else
+        {
+
+            if (leftIKFootSolver.enabled == false)
+            {
+                leftIKFootSolver.enabled = true;
+                rightIKFootSolver.enabled = true;
+            }
+        }
+    }
+}

0e990c533f2c7d8910fa73123c63e64ca115b839
jsm0325 <jsm961207@naver.com>
2024-05-30 21:53:37 +0900

    플레이어 이동, 손목 UI, UI 레이저 포인터 추가

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,11 +105,11 @@ namespace Valve.VR
             }
         }
         
-        public virtual SteamVR_Action_Vector2 Move
+        public virtual SteamVR_Action_Vector2 TouchpadPosition
         {
             get
             {
-                return SteamVR_Actions.default_Move;
+                return SteamVR_Actions.default_TouchpadPosition;
             }
         }
         

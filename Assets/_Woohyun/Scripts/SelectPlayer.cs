78642c561b3482c7683d0602889a790fc9c656aa
jsm0325 <jsm961207@naver.com>
2024-06-07 15:57:56 +0900

    타이틀씬 추가

--- Assets/_Woohyun/Scripts/SelectPlayer.cs
+++ Assets/_Woohyun/Scripts/SelectPlayer.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Valve.VR;
 using Valve.VR.Extras;
+using UnityEngine.SceneManagement;
 
 public class SelectPlayer : MonoBehaviour
 {
@@ -17,7 +18,6 @@ public class SelectPlayer : MonoBehaviour
 
     public SteamVR_LaserPointer steamVR_LaserPointer;
     public SteamVR_Behaviour_Pose poseBehaviour;
-    public SteamVR_Action_Boolean fireAction; // 버튼 입력을 감지할 SteamVR 액션
     public SteamVR_Input_Sources handType;
 
 
@@ -26,7 +26,8 @@ public class SelectPlayer : MonoBehaviour
     public AudioClip clickSound;
 
 
-    private bool restart = false;
+    private bool btn_mode1 = false;
+    private bool btn_mode2 = false;
     //public AudioClip fireSound;
 
     void Start()
@@ -51,104 +52,43 @@ public class SelectPlayer : MonoBehaviour
 
     private void OnPointerIn(object sender, PointerEventArgs e)
     {
-        /* 다시하기 */
-        if (e.target.gameObject.CompareTag("Restart") == true)
+        /* 모드 1 */
+        if (e.target.gameObject.CompareTag("btn_mode1") == true)
         {
-            restart = true;
+            btn_mode1 = true;
         }
         else
         {
-            restart = false;
+            btn_mode1 = false;
+        }
+
+        /* 모드 2 */
+        if (e.target.gameObject.CompareTag("btn_mode2") == true)
+        {
+            btn_mode2 = true;
+        }
+        else
+        {
+            btn_mode2 = false;
         }
 
     }
 
     private void OnPointerClick(object sender, PointerEventArgs e)
     {
-        if (nearbyItem != null)
+        if (btn_mode1 == true)
         {
-            PickupItem();
+            SceneManager.LoadScene("OutMap");
         }
-        else if (quickSlotIndex != -1)
+        else if (btn_mode2 == true)
         {
-            DropItem(quickSlotIndex);
-        }
-        else if (nearbyDayAdvanceTrigger != null)
-        {
-            DayPass();
-        }
-        else if (restart == true)
-        {
-            GameManager.Instance.ReStartNewGame();
-        }
-    }
-
-    private void OnFireAction(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
-    {
-        if (fromSource == SteamVR_Input_Sources.RightHand) // 오른손 컨트롤러에서 발생한 이벤트인지 확인
-        {
-            FirebBullet.Shoot();
+            SceneManager.LoadScene("SciFi_Warehouse");
         }
     }
 
     void Update()
     {
-        if (GameManager.Instance.isGameOver)
-        {
-            return;
-        }
-        //CheckForDayAdvanceTrigger();
-
-       if (currentHP <= 0)
-        {
-            GameManager.Instance.GameOver();
-        }
 
     }
 
-    void PickupItem()
-    {
-        if (nearbyItem != null)
-        {
-            if (UIManager.Instance.IsQuickSlotFull())
-            {
-                GameManager.Instance.PlaySound(quickSlotfullSound);
-                Debug.Log("퀵슬롯 꽉 참");
-                return;
-            }
-
-            int slotIndex = UIManager.Instance.AddItemToQuickSlot(nearbyItem);
-            if (slotIndex != -1)
-            {
-                GameManager.Instance.PlaySound(pickupSound);
-                InventoryManager.Instance.AddItem(nearbyItem); // 인벤토리에 아이템 추가
-                nearbyItem.Pickup(); // 아이템 오브젝트 비활성화
-                nearbyItem = null;
-                UIManager.Instance.ShowItemInfo(false);
-            }
-        }
-    }
-
-    void DropItem(int slotIndex)
-    {
-        Item itemToDrop = UIManager.Instance.GetSelectedQuickSlotItem(slotIndex);
-        if (itemToDrop != null)
-        {
-            GameManager.Instance.PlaySound(dropSound);
-            Vector3 dropPosition = mainCamera.transform.position + mainCamera.transform.forward * 2.0f;
-            itemToDrop.Drop(dropPosition);
-            InventoryManager.Instance.RemoveItem(itemToDrop); // 인벤토리에서 아이템 제거 및 씬으로 반환
-            UIManager.Instance.RemoveItemFromQuickSlot(slotIndex);
-            Debug.Log($"{slotIndex + 1}번 슬롯 아이템 드랍");
-        }
-    }
-
-    // 하루 넘기는 버튼에서 F키 눌렀을 때 처리
-    void DayPass()
-    {
-        GameManager.Instance.DayPass();
-        GameManager.Instance.PlaySound(dayPassSound);
-        nearbyDayAdvanceTrigger = null;
-        UIManager.Instance.ShowInteractionMessage("", false);
-    }
 }
